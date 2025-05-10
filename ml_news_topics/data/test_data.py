b122dd74e3a9cd0fa1148430c9e37476c6d5183b
Гурин Артур <arturgurin39@gmail.com>
2025-05-10 21:22:31 +0300

    добавлены тестовые данные

---
+++ ml_news_topics/data/test_data.py
@@ -0,0 +1,46 @@
+news_data = [
+    {
+        "link": "https://www.huffpost.com/entry/covid-boosters-uptake-us_n_632d719ee4b087fae6feaac9",
+        "headline": "Over 4 Million Americans Roll Up Sleeves For Omicron-Targeted COVID Boosters",
+        "category": "U.S. NEWS",
+        "short_description": "Health experts said it is too early to predict whether demand would match up with the 171 million doses of the new boosters the U.S. ordered for the fall.",
+        "authors": "Carla K. Johnson, AP",
+        "date": "2022-09-23"
+    },
+    {
+        "link": "https://www.huffpost.com/entry/american-airlines-passenger-banned-flight-attendant-punch-justice-department_n_632e25d3e4b0e247890329fe",
+        "headline": "American Airlines Flyer Charged, Banned For Life After Punching Flight Attendant On Video",
+        "category": "U.S. NEWS",
+        "short_description": "He was subdued by passengers and crew when he fled to the back of the aircraft after the confrontation, according to the U.S. attorney's office in Los Angeles.",
+        "authors": "Mary Papenfuss",
+        "date": "2022-09-23"
+    },
+    {
+        "link": "https://www.huffpost.com/entry/funniest-tweets-cats-dogs-september-17-23_n_632de332e4b0695c1d81dc02",
+        "headline": "23 Of The Funniest Tweets About Cats And Dogs This Week (Sept. 17-23)", "category": "COMEDY",
+        "short_description": "\"Until you have a dog you don't understand what could be eaten.\"",
+        "authors": "Elyse Wanshel", "date": "2022-09-23"
+    },
+    {
+        "link": "https://www.huffpost.com/entry/funniest-parenting-tweets_l_632d7d15e4b0d12b5403e479",
+        "headline": "The Funniest Tweets From Parents This Week (Sept. 17-23)", "category": "PARENTING",
+        "short_description": "\"Accidentally put grown-up toothpaste on my toddler\u2019s toothbrush and he screamed like I was cleaning his teeth with a Carolina Reaper dipped in Tabasco sauce.\"",
+        "authors": "Caroline Bologna", "date": "2022-09-23"},
+    {
+        "link": "https://www.huffpost.com/entry/amy-cooper-loses-discrimination-lawsuit-franklin-templeton_n_632c6463e4b09d8701bd227e",
+        "headline": "Woman Who Called Cops On Black Bird-Watcher Loses Lawsuit Against Ex-Employer",
+        "category": "U.S. NEWS",
+        "short_description": "Amy Cooper accused investment firm Franklin Templeton of unfairly firing her and branding her a racist after video of the Central Park encounter went viral.",
+        "authors": "Nina Golgowski", "date": "2022-09-22"},
+    {
+        "link": "https://www.huffpost.com/entry/belk-worker-found-dead-columbiana-centre-bathroom_n_632c5f8ce4b0572027b0251d",
+        "headline": "Cleaner Was Dead In Belk Bathroom For 4 Days Before Body Found: Police", "category": "U.S. NEWS",
+        "short_description": "The 63-year-old woman was seen working at the South Carolina store on Thursday. She was found dead Monday after her family reported her missing, authorities said.",
+        "authors": "", "date": "2022-09-22"},
+    {
+        "link": "https://www.huffpost.com/entry/reporter-gets-adorable-surprise-from-her-boyfriend-while-working-live-on-tv_n_632ccf43e4b0572027b10d74",
+        "headline": "Reporter Gets Adorable Surprise From Her Boyfriend While Live On TV", "category": "U.S. NEWS",
+        "short_description": "\"Who's that behind you?\" an anchor for New York\u2019s PIX11 asked journalist Michelle Ross as she finished up an interview.",
+        "authors": "Elyse Wanshel", "date": "2022-09-22"
+    },
+]
