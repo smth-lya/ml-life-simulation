0f0ef58d75ce54bf2feec058b2602ee207e8b4b4
Egor <egor_udir@mail.ru>
2025-05-11 12:31:40 +0300

    сделана токенизация и сбор уникальных слов в словарь

---
+++ ml_news_topics/bootstrap_nltk.py
@@ -0,0 +1,6 @@
+﻿# выполнить в консоли - pip install nltk
+# выполнить этот файл 1 раз для установки библиотек
+import nltk
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
--- ml_news_topics/main.py
+++ ml_news_topics/main.py
@@ -1,1 +1,43 @@
-print('hello world!')
+import json
+from stop_words import stop_words
+from nltk.stem import WordNetLemmatizer, PorterStemmer
+
+lemmatizer = WordNetLemmatizer()
+stemmer = PorterStemmer()
+
+def load_articles_from_json(filepath):
+    articles = []
+    with open(filepath, 'r', encoding='utf-8') as f:
+        for line in f:
+            data = json.loads(line)
+            articles.append(data)
+    return articles
+
+def clean_and_tokenize(text):
+    text = text.lower()
+    cleaned = "".join(char if char.isalpha() or char == " " else " " for char in text)
+    words = cleaned.split()
+
+    tokens = []
+    for word in words:
+        if word not in stop_words:
+            lemma = lemmatizer.lemmatize(word)
+            stemmed = stemmer.stem(lemma)
+            tokens.append(stemmed)
+
+    return tokens
+
+
+articles = load_articles_from_json("../News_Category_Dataset_v3.json")
+all_tokens = []
+
+for article in articles:
+    description = article.get("short_description", "")
+    tokens = clean_and_tokenize(description)
+    all_tokens.extend(tokens)
+
+unique_tokens = sorted(set(all_tokens))
+word_dict = {word: idx for idx, word in enumerate(unique_tokens)}
+
+for word, idx in word_dict.items():
+    print(f'"{word}": {idx},')
